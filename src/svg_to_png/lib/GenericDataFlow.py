2249002c632aced036a78a53df691080183f3092
Aamir <48929123+heyitsaamir@users.noreply.github.com>
2024-07-29 17:06:19 -0700

    Improve arrows and prompt (#4)

--- src/svg_to_png/lib/GenericDataFlow.py
+++ src/svg_to_png/lib/GenericDataFlow.py
@@ -3,45 +3,50 @@ import drawsvg as draw
 from .Curve import Curve
 from .utils import calculate_size
 
+def interpolate_quadratic_bezier(start, control, end):
+    def interpolator(t):
+        return [
+            (math.pow(1 - t, 2) * start[0]) +
+            (2 * (1 - t) * t * control[0]) +
+            (math.pow(t, 2) * end[0]),
+            (math.pow(1 - t, 2) * start[1]) +
+            (2 * (1 - t) * t * control[1]) +
+            (math.pow(t, 2) * end[1]),
+        ]
+    return interpolator
+
+def interpolate_quadratic_bezier_angle(start, control, end):
+    def interpolator(t):
+        tangent_x = (2 * (1 - t) * (control[0] - start[0])) + \
+                    (2 * t * (end[0] - control[0]))
+        tangent_y = (2 * (1 - t) * (control[1] - start[1])) + \
+                    (2 * t * (end[1] - control[1]))
+
+        return math.atan2(tangent_y, tangent_x) * (180 / math.pi)
+    return interpolator
+
 class GenericDataFlow(Curve):
     def add_curve(self, d):
         curve = draw.Path(fill="none", stroke="black").M(self.sourceX, self.sourceY).Q(self.controlX, self.controlY, self.targetX, self.targetY)
         d.append(curve)
         
-    def add_arrow(self, d):
-        coordinates = [
-            (self.targetX - 8.5, self.targetY - 4.1),
-            (self.targetX + 0.5, self.targetY - 1.1),
-            (self.targetX - 8.5, self.targetY + 1.9)
-        ]
-
-        axisX = -self.targetX
-        axisY = 0
-        slopeX = self.controlX - self.targetX
-        slopeY = self.controlY - self.targetY
-        dotProduct = axisX * slopeX + axisY * slopeY
-        axisLength = abs(axisX)
-        slopeLength = math.sqrt(slopeX**2 + slopeY**2)
-        angle = (math.acos(dotProduct / axisLength / slopeLength) * 180) / math.pi
-        if slopeY > 0:
-            angle = -angle
-        angle_rad = math.radians(angle)
-        cos_angle = math.cos(angle_rad)
-        sin_angle = math.sin(angle_rad)
-        translate_x = self.targetX + 0.5
-        translate_y = self.targetY - 1.1
-
-        matrix = (
-            cos_angle, sin_angle, -sin_angle, cos_angle,
-            translate_x - cos_angle * translate_x + sin_angle * translate_y,
-            translate_y - sin_angle * translate_x - cos_angle * translate_y
-        )
-
-        points_str = ' '.join([f"{x},{y}" for x, y in coordinates])
-        transform_str = f"matrix({matrix[0]},{matrix[1]},{matrix[2]},{matrix[3]},{matrix[4]},{matrix[5]})"
-        polyline_str = f'<polyline points="{points_str}" fill="none" stroke="black" transform="{transform_str}" />'
-        polyline = draw.Raw(polyline_str)
-        d.append(polyline)
+    def add_additional_arrows(self, d):
+        quadratic_interpolator = interpolate_quadratic_bezier([self.sourceX, self.sourceY], [self.controlX, self.controlY], [self.targetX, self.targetY])
+        # pts = 10
+        # for i in range(pts):
+        #     t = i / (pts - 1)
+        #     x, y = quadratic_interpolator(t)
+        #     # angle = interpolate_quadratic_bezier_angle([self.sourceX, self.sourceY], [self.controlX, self.controlY], [self.targetX, self.targetY])(t)
+        #     d.append(draw.Circle(x, y, 3, fill="black"))
+            
+        quadratic_angle_interpolator = interpolate_quadratic_bezier_angle([self.sourceX, self.sourceY], [self.controlX, self.controlY], [self.targetX, self.targetY])
+        pts = 4
+        for i in range(pts):
+            t = i / (pts - 1)
+            x, y = quadratic_interpolator(t)
+            angle = quadratic_angle_interpolator(t)
+            path = draw.Path(fill="black", transform=f"translate({x}, {y}) rotate({angle})").M(12, 0).L(-5, -8).L(0, 0).L(-5, 8).Z()
+            d.append(path)
         
     def add_text(self, d):
         if not self.name:
@@ -56,3 +61,5 @@ class GenericDataFlow(Curve):
         self.add_curve(d)
         self.add_text(d)
         self.add_text(d)
+        # self.add_arrow(d)
+        self.add_additional_arrows(d)
