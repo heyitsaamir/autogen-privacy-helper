58dc6c9512a01ff246ab86c47e2e09049edac431
Aamir <48929123+heyitsaamir@users.noreply.github.com>
2024-08-01 12:48:24 -0700

    Add rag via nested chats (#1)
    * Add rag agent
    
    * Fix
    
    * Use nested chats
    
    * Rename
    
    * Bump up teams-ai version. Use list for custom_speaker_selection
    
    * Update
    
    * Fix Rag searcher
    
    * Update rag
    
    * Update

---
+++ src/rag_agents.py
@@ -0,0 +1,169 @@
+from datetime import datetime, timedelta
+import re
+from typing import List, Union, Tuple
+from autogen.agentchat.contrib.vectordb.base import QueryResults, VectorDB, Document
+from autogen.agentchat.contrib.retrieve_user_proxy_agent import RetrieveUserProxyAgent
+from autogen.agentchat.contrib.retrieve_assistant_agent import RetrieveAssistantAgent
+from autogen import AssistantAgent, ConversableAgent
+from azure.core.credentials import AzureKeyCredential
+from azure.search.documents import SearchClient
+from azure.core.exceptions import ResourceNotFoundError
+from config import Config
+
+def build_config(suffix: str, previous_day_index: int = 0):
+    # Get the current date
+    date = datetime.now()
+    date = date - timedelta(days=previous_day_index)
+
+    # Format the date as YYYYMMDD
+    formatted_date = date.strftime("%Y%m%d")
+
+    # Construct the final string
+    return f"{formatted_date}-{suffix}"
+    
+
+class AzureAISearch(VectorDB):
+    def create_collection(self, collection_name: str, overwrite: bool = False, get_or_create: bool = True):
+        pass
+    
+    def get_collection(self, collection_name: str = None):
+        pass
+    
+    def delete_collection(self, collection_name: str):
+        pass
+    
+    def insert_docs(self, docs: List[Document], collection_name: str = None, upsert: bool = False, **kwargs):
+        pass
+    
+    def update_docs(self, docs: List[Document], collection_name: str = None, **kwargs):
+        pass
+    
+    def delete_docs(self, doc_ids: List[str], collection_name: str = None):
+        pass
+    
+    def get_docs_by_ids(self, doc_ids: List[str], collection_name: str = None):
+        pass
+    
+    def retrieve_docs(self,
+        queries: List[str],
+        collection_name: Union[str, None] = None,
+        n_results: int = 10,
+        distance_threshold: float = 0,
+        **kwargs,
+    ) -> QueryResults:
+        documents_all: List[List[Tuple[Document, float]]] = []
+        count = 0
+        for query in queries:
+            documents: List[Tuple[Document, float]] = []
+            for result in self._search(query):
+                documents.append(({
+                    "id": result["id"],
+                    "content": " ".join(c.text for c in result["@search.captions"]),
+                    "metadata": None,
+                    "embedding": None
+                }, result["@search.score"]))
+                count = count + 1
+            documents_all.append(documents)
+        return documents_all
+    
+    def _search(self, query: str, previous_day_index: int = 0):
+        print("Performing search!")
+        search_endpoint = Config.AZURE_SEARCH_SERVICE_ENDPOINT
+        # index name is YYYYMMDD-1-home-index
+        index_name = build_config("1-home-index", previous_day_index=previous_day_index)
+        api_key = Config.AZURE_SEARCH_API_KEY
+        semantic_search_config = build_config("1-home-index-sc", previous_day_index=previous_day_index)
+        
+        if not api_key:
+            raise ValueError("No Azure Search API key provided.")
+        
+        search_client = SearchClient(search_endpoint, index_name, AzureKeyCredential(api_key))
+        try:
+            raw_response = search_client.search(search_text=query,
+                query_type="semantic",
+                semantic_configuration_name=semantic_search_config,
+                query_caption="extractive",
+                query_answer="extractive|count-3",
+                top=5)
+            response = list(raw_response)
+        except Exception as e:
+            if previous_day_index <= 2 and isinstance(e, ResourceNotFoundError):
+                print(f"Resource not found error. Retrying with previous day. (curr_day - {previous_day_index})")
+                return self._search(query, previous_day_index=previous_day_index + 1)
+            raise e
+            
+        output = []
+        for result in response:
+            output.append(result)
+        return output
+
+def setup_rag_assistant(llm_config):
+    db = AzureAISearch()
+    rag_proxy_agent = RetrieveUserProxyAgent(
+        name="rag_proxy_agent",
+        human_input_mode="NEVER",
+        retrieve_config={
+            "task": "qa",
+            "vector_db": db,
+        },
+    )
+    
+    rag_assistant_agent = RetrieveAssistantAgent(
+        name="rag_assistant",
+        system_message="You are a helpful assistant.",
+        llm_config=llm_config,
+    )
+    
+    assistant = AssistantAgent(
+        name="System_Details_Answerer",
+        description="A system details answerer agent that can answer questions about the overall system.",
+    )
+    
+    def extract_problem(message):
+        match = re.search(r'<QUESTION[^>]*>\s*(.*?)\s*</QUESTION>', message)
+        if match:
+            return match.group(1)
+        return message
+        
+    def message_generator(_recipient, messages, sender, _config):
+        last_msg = messages[-1].get("content")
+        if sender is assistant:
+            _context = {"problem": extract_problem(last_msg), "n_results": 3}
+            return rag_proxy_agent.message_generator(rag_proxy_agent, messages, _context)
+        else:
+            return last_msg
+    
+    rag_assistant_agent.register_nested_chats([
+        {
+            "recipient": rag_assistant_agent,
+            "sender": rag_proxy_agent,
+            "summary_method": "last_msg",
+            "message": message_generator
+        },
+    ], trigger=assistant)
+    
+    def trigger(sender):
+        return sender not in [assistant] # To prevent the assistant from triggering itself
+    
+    def custom_summary_method(
+                sender: ConversableAgent,
+                recipient: ConversableAgent,
+                summary_args: dict,
+            ):
+                last_msg = recipient.last_message(sender)
+                if last_msg:
+                    last_msg_content = last_msg.get("content", None)
+                    if last_msg_content == '':
+                        return 'I do not know.'
+                return last_msg
+    
+    assistant.register_nested_chats([
+        {
+            "recipient": rag_assistant_agent,
+            "sender": assistant,
+            "summary_method": custom_summary_method,
+            "max_turns": 1,
+        },
+    ], trigger=trigger)
+        
+    return assistant
--- src/threat_model_reviewer_group.py
+++ src/threat_model_reviewer_group.py
@@ -1,13 +1,14 @@
 import io
 from typing import Union
 from PIL import Image
-from autogen import AssistantAgent, GroupChat, Agent
+from autogen import AssistantAgent, GroupChat, Agent, ConversableAgent
 from autogen.agentchat.contrib.multimodal_conversable_agent import MultimodalConversableAgent
 
 from botbuilder.core import TurnContext
 from teams.input_file import InputFile
 
 from state import AppTurnState
+from rag_agents import setup_rag_assistant
 from svg_to_png.svg_to_png import convert_svg_to_png
 
 
@@ -17,7 +18,6 @@ def get_image(input_file: Union[InputFile, str]):
     hsize = int((float(img.size[1]) * float(wpercent)))
     img = img.resize((400, hsize))
     return img
-
 
 class ImageReasoningAgent(MultimodalConversableAgent):
     def __init__(self, img: Union[Image.Image, None], extra_details: Union[str, None] = None, *args, **kwargs):
@@ -40,6 +40,9 @@ class ImageReasoningAgent(MultimodalConversableAgent):
                         "text": f"Here are some helpful labels: {self.extra_details}. Use these to help answer the questions."
                     })
                 messages.append({"content": img_message, "role": "user"})
+            else:
+                messages = messages.copy()
+                messages.append({"content": "No threat model exists.", "role": "user"})
             return messages
         self.hook_lists["process_all_messages_before_reply"].append(
             add_image_to_messages)
@@ -51,6 +54,7 @@ class ThreatModelReviewerGroup:
 2. It should be clear to tell what each red boundary is.
 3. All arrows should be labeled (labels are inside green boxes).
 4. All labels for the arrows should have sequential numbers. These numbers indicate the order in which the flow happens. If all arrows do not contains labels, indicate which ones. Otherwise state the flow of data in the order that the arrow point
+5. All nodes should correspond to a valid service or data store in the system. Are you able to verify that all the nodes exist in the system?
 """):
         self.llm_config = llm_config
         self.threat_model_spec = threat_model_spec
@@ -70,6 +74,7 @@ When asking the question, you should format your question in this format:
 Your question
 </QUESTION>
 
+To ask if nodes are for a valid service or data store in the system, ask one question at a time for each node. Be specific (eg. use the node name in the question).
 If you have no questions to ask, say "NO_QUESTIONS" and nothing else.
             """,
             description="A questioner agent that can ask questions based on a threat model picture. It can ask questions about the threat model from the given picture or about the broader system.",
@@ -95,11 +100,12 @@ If you have no questions to ask, say "NO_QUESTIONS" and nothing else.
                                     img_details = img_details + f"\n{key}: {label}" if img_details else f"{key}: {label}"
                             
         answerer_agent = ImageReasoningAgent(
-            name="Threat_Model_Answerer",
-            system_message="""You are an answerer agent.
+            name="Threat_Model_Image_Answerer",
+            system_message="""You are an threat model answerer agent.
 Your role is to answer questions based on the threat model picture.
 If you do not have a threat model, ask the user to provide one.
 You will *never* speculate or infer anything that is not in the threat model picture.
+The threat model indicates the flow of data in a bigger system. You do not have any context about the system, but you can answer questions regarding the data flow present in the threat model.
 Answer the questions as clearly and concisely as possible.
 
 If you do not understand something from the threat model picture, you may ask a clarifying question. In case of a clarifying question for the user, put your exact question between tags in this format:
@@ -117,24 +123,27 @@ your clarifying question
         answer_evaluator_agent = AssistantAgent(
             name="Overall_spec_evaluator",
             system_message=f"""You are an answer reviewer agent.
-Your role is to evaluate the answers given by the Threat_Model_Answerer agent.
+Your role is to evaluate the answers given by the Threat_Model_Answerer agent and the System_Details_Answerer agent.
 You are only called if the Questioner agent has no more questions to ask.
 Provide details on the quality of the threat model based on the answers given by the answerer agent.
 Evaluate the answers based on the following spec criteria:
 {self.threat_model_spec}
 For each spec criteria that is not met, provide some action items on how to improve the threat model to meet the requirement.
             """,
+            description="An answer evaluator agent that can evaluate the answers given by the Threat_Model_Answerer and System_Details_Answerer agents.",
             llm_config={"config_list": [self.llm_config],
                         "timeout": 60, "temperature": 0},
             description="An answer evaluator agent that can evaluate the answers given by the Threat_Model_Answerer agent.",
         )
+        
+        rag_assistant = setup_rag_assistant(llm_config=self.llm_config)
 
-        for agent in [questioner_agent, answerer_agent, answer_evaluator_agent]:
+        for agent in [questioner_agent, answerer_agent, answer_evaluator_agent, rag_assistant]:
             group_chat_agents.append(agent)
 
         def custom_speaker_selection_func(
             last_speaker: Agent, groupchat: GroupChat
-        ) -> Union[Agent, str, None]:
+        ) -> Union[Agent, str, list[Agent], None]:
             last_message = groupchat.messages[-1]
             content = last_message.get("content")
             if last_speaker == questioner_agent:
@@ -142,7 +151,7 @@ For each spec criteria that is not met, provide some action items on how to impr
                     print("No questions, so moving to answer evaluator")
                     return answer_evaluator_agent
                 else:
-                    return answerer_agent
+                    return 'auto'
 
             if last_speaker == answerer_agent:
                 last_message = groupchat.messages[-1]
@@ -151,6 +160,12 @@ For each spec criteria that is not met, provide some action items on how to impr
                     return user_agent
                 else:
                     return questioner_agent
+                
+            # ## If the last speaker is the rag_assistant and it has just provided a tool response,
+            # ## then we want to convert that into a user message.
+            # if last_speaker == rag_assistant and content and last_message.get("tool_responses"):
+            #     return rag_assistant
+            
             return 'auto'
 
         groupchat = GroupChat(
@@ -160,8 +175,9 @@ For each spec criteria that is not met, provide some action items on how to impr
             speaker_selection_method=custom_speaker_selection_func,
             allowed_or_disallowed_speaker_transitions={
                 user_agent: [questioner_agent],
-                questioner_agent: [answerer_agent, answer_evaluator_agent],
-                answerer_agent: [user_agent, questioner_agent],
+                questioner_agent: [answerer_agent, rag_assistant],
+                rag_assistant: [questioner_agent],
+                answerer_agent: [questioner_agent, user_agent],
                 answer_evaluator_agent: [user_agent]
             },
             speaker_transitions_type="allowed"
